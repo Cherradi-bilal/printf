e224959297b9339bada28b4a33a8b27574aaef3e
cherradi bilal <87195059+Cherradi-bilal@users.noreply.github.com>
2023-04-25 21:49:39 +0100

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
- * get_bilal3 - Calculates active bilal3
+ * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
-int get_bilal3(const char *format, int *i)
+int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int bilal1, bilal2;
-	int bilal3 = 0;
+	int j, curr_i;
+	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (bilal2 = *i + 1; format[bilal2] != '\0'; bilal2++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (bilal1 = 0; FLAGS_CH[bilal1] != '\0'; bilal1++)
-			if (format[bilal2] == FLAGS_CH[bilal1])
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
 			{
-				bilal3 |= FLAGS_ARR[bilal1];
+				flags |= FLAGS_ARR[j];
 				break;
 			}
 
-		if (FLAGS_CH[bilal1] == 0)
+		if (FLAGS_CH[j] == 0)
 			break;
 	}
 
-	*i = bilal2 - 1;
+	*i = curr_i - 1;
 
-	return (bilal3);
+	return (flags);
 }
+
