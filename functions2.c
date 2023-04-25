a9d40c59084394d75151c09197bc00cd26dccb32
cherradi bilal <87195059+Cherradi-bilal@users.noreply.github.com>
2023-04-25 21:34:53 +0100

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -4,15 +4,15 @@
 /**
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @bilal2: Buffer array to handle print
+ * @bilal3:  Calculates active bilal3
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed.
  */
-int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_pointer(bilal1, char bilal2[],
+	int bilal3, int width, int precision, int size)
 {
 	char extra_c = 0, padd = ' ';
 	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
@@ -26,50 +26,50 @@ int print_pointer(va_list types, char buffer[],
 	if (addrs == NULL)
 		return (write(1, "(nil)", 5));
 
-	buffer[BUFF_SIZE - 1] = '\0';
+	bilal2[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
 
 	num_addrs = (unsigned long)addrs;
 
 	while (num_addrs > 0)
 	{
-		buffer[ind--] = map_to[num_addrs % 16];
+		bilal2[ind--] = map_to[num_addrs % 16];
 		num_addrs /= 16;
 		length++;
 	}
 
-	if ((flags & F_ZERO) && !(flags & F_MINUS))
+	if ((bilal3 & F_ZERO) && !(bilal3 & F_MINUS))
 		padd = '0';
-	if (flags & F_PLUS)
+	if (bilal3 & F_PLUS)
 		extra_c = '+', length++;
-	else if (flags & F_SPACE)
+	else if (bilal3 & F_SPACE)
 		extra_c = ' ', length++;
 
 	ind++;
 
-	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
-	return (write_pointer(buffer, ind, length,
-		width, flags, padd, extra_c, padd_start));
+	/*return (write(1, &bilal2[i], BUFF_SIZE - i - 1));*/
+	return (write_pointer(bilal2, ind, length,
+		width, bilal3, padd, extra_c, padd_start));
 }
 
 /************************* PRINT NON PRINTABLE *************************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
  * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @bilal2: Buffer array to handle print
+ * @bilal3:  Calculates active bilal3
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_non_printable(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_non_printable(bilal1, char bilal2[],
+	int bilal3, int width, int precision, int size)
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
 
-	UNUSED(flags);
+	UNUSED(bilal3);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
@@ -80,38 +80,38 @@ int print_non_printable(va_list types, char buffer[],
 	while (str[i] != '\0')
 	{
 		if (is_printable(str[i]))
-			buffer[i + offset] = str[i];
+			bilal2[i + offset] = str[i];
 		else
-			offset += append_hexa_code(str[i], buffer, i + offset);
+			offset += append_hexa_code(str[i], bilal2, i + offset);
 
 		i++;
 	}
 
-	buffer[i + offset] = '\0';
+	bilal2[i + offset] = '\0';
 
-	return (write(1, buffer, i + offset));
+	return (write(1, bilal2, i + offset));
 }
 
 /************************* PRINT REVERSE *************************/
 /**
  * print_reverse - Prints reverse string.
  * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @bilal2: Buffer array to handle print
+ * @bilal3:  Calculates active bilal3
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Numbers of chars printed
  */
 
-int print_reverse(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_reverse(bilal1, char bilal2[],
+	int bilal3, int width, int precision, int size)
 {
 	char *str;
 	int i, count = 0;
 
-	UNUSED(buffer);
-	UNUSED(flags);
+	UNUSED(bilal2);
+	UNUSED(bilal3);
 	UNUSED(width);
 	UNUSED(size);
 
@@ -139,15 +139,15 @@ int print_reverse(va_list types, char buffer[],
 /**
  * print_rot13string - Print a string in rot13.
  * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @bilal2: Buffer array to handle print
+ * @bilal3:  Calculates active bilal3
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Numbers of chars printed
  */
-int print_rot13string(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_rot13string(bilal1, char bilal2[],
+	int bilal3, int width, int precision, int size)
 {
 	char x;
 	char *str;
@@ -157,8 +157,8 @@ int print_rot13string(va_list types, char buffer[],
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	str = va_arg(types, char *);
-	UNUSED(buffer);
-	UNUSED(flags);
+	UNUSED(bilal2);
+	UNUSED(bilal3);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
